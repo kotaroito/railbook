aa4dbe76b88bbdc9217b882d83c32a757e2c0cc8
kotaroito <kotaroito@gmail.com>
2014-09-29 08:52:25 +0900

    update

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,15 @@
 class Book < ActiveRecord::Base
+    validates :isbn,
+        presence: true,
+        uniqueness: true
+    validates :title,
+        presence: true,
+        length: { minimum: 1, maximum: 100 },
+        uniqueness: { scope: :publish }
+    validates :price,
+        numericality: { only_integer: true, less_than: 10000 }
+    validates :publish,
+        inclusion: { in: ['技術評論社', '翔泳社', '秀和システム', '日経BP社', 'ソシム'] }
     scope :publisher, ->(pub) {
         where(publish: pub).order(published: :desc).limit(10)
     }
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ActiveRecord::Base
+    validates :agreement,
+        acceptance: { on: :create }
+    validates :email,
+        confirmation: true,
+        presence: { unless: 'dm.blank?' }
 end
